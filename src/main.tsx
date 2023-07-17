552ff5f21427a87b37cac261c6f44074f6f69edb
Rythen-Winds <112634836+Rythen-Winds@users.noreply.github.com>
2023-07-17 11:23:39 -0400

    feat: Add react-router-dom and update main.ts
    - Import createBrowserRouter, RouterProvider from react-router-dom
    - Add routes for Main_Menu and Character_Menu components
    - Update ReactDOM.createRoot rendering to include RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main_Menu from "./components/Main_Menu.tsx";
+import Character_Menu from "./components/Character_Menu.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main_Menu />,
+  },
+  {
+    path: "/character_menu",
+    element: <Character_Menu />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
